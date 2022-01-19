7e2fdb6089d86f7fce617fb3fc54877fdb5e4860
Nicolas Helman <nicolashelman11@gmail.com>
2022-01-19 19:03:25 -0300

    Agregando tareas de prueba

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Primer Tarea",
+        day: "Enero 19 a las 7:00 pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Segunda Tarea",
+        day: "Enero 19 a las 9:00 pm",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Tercer Tarea",
+        day: "Enero 19 a las 11:00 pm",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Cuarta Tarea",
+        day: "Enero 20 a las 7:00 am",
+        reminder: false
+    },
+]
